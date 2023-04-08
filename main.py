03c9e6d01abeac473cd22c596eba12b0ea37cdc7
Eric Chang <82834722+echang505@users.noreply.github.com>
2023-04-08 02:46:14 -0400

    Added main file

--- main.py
+++ main.py
@@ -1,54 +1,69 @@
 import cv2
-import os
 import numpy as np
+import os
 
-print(cv2.__version__)
-
-# path to the dataset
-dataset_path = 'Images'
-
-# initialize the recognizer
+# Load the saved model
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('trained_data.yml')
 
-# create a list to store faces and labels
-faces = []
-labels = []
+# Set the video file path
+video_path = 'Videos/OMG_Trim.mp4'
 
-# loop through all folders and images in the dataset path
-for folder_name in os.listdir(dataset_path):
-    folder_path = os.path.join(dataset_path, folder_name)
-    if not os.path.isdir(folder_path):
-        continue
-    
-    label = int(list(word[0] for word in folder_name.split())[0])
-    
-    # loop through all images in the folder
-    for file_name in os.listdir(folder_path):
-        image_path = os.path.join(folder_path, file_name)
-        if not os.path.isfile(image_path):
-            continue
-        try:
+# Set the output video file path
+output_path = 'VideoResult/OMG_Trim.mp4'
 
-            # read the image and convert it to grayscale
-            image = cv2.imread(image_path)
-            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            print(f"image read: {image_path}")
+# Open the video file
+cap = cv2.VideoCapture(video_path)
 
-        except: 
-            print(f"Could not read image: {image_path}")
-            continue
+# Get the video frame rate
+frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
 
-        # detect faces in the image
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        faces_rect = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-        
-        # loop through all detected faces and add them to the list of faces
-        for (x, y, w, h) in faces_rect:
-            faces.append(gray[y:y+h, x:x+w])
-            labels.append(label)
+# Get the video frame size
+frame_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
-# train the recognizer with the faces and labels
-recognizer.train(faces, np.array(labels))
+# Create a video writer object
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(output_path, fourcc, frame_rate, frame_size)
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-# save the trained recognizer to a file
-recognizer.save('trained_data.yml')
+frame_count = 0
+# Process each frame
+while True:
+    # Read a frame from the video
+    ret, frame = cap.read()
+    frame_count += 1
+    if not ret:
+        break
+    print("frame number: " + str(frame_count) + " / " + str(total_frames))
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the frame
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces_rect = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+
+    # Recognize faces in the frame and draw labels
+    for (x, y, w, h) in faces_rect:
+        face_roi = gray[y:y+h, x:x+w]
+        label, confidence = recognizer.predict(face_roi)
+
+        if label == 1:
+            new_label = "dani"
+        elif label == 2:
+            new_label = "haerin"
+        elif label == 3:
+            new_label = "hanni"
+        elif label == 4:
+            new_label = "hyein"
+        elif label == 5:
+            new_label = "minji"
+
+        cv2.putText(frame, new_label, (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Write the frame to the output video file
+    out.write(frame)
+
+# Release the video capture and writer objects
+cap.release()
+out.release()
